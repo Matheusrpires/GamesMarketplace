fdac47cafc54fc43f6ce0cd0e463c162d032ef7f
Matheus Rodrigues <matheusrp97@hotmail.com>
2022-02-06 16:35:30 -0300

    Highlight Developing component highligh and adding basic images

--- src/components/Highlight/index.tsx
+++ src/components/Highlight/index.tsx
@@ -2,24 +2,33 @@ import Button from 'components/Button'
 import * as S from './styles'
 
 export type HighLightProps = {
+  alignment?: 'right' | 'left'
   title: string
   subtitle: string
+  backgroundImage: string
+  floatImage?: string
   buttonLabel: string
   buttonLink: string
 }
 
 const Highlight = ({
+  alignment = 'right',
   title,
   subtitle,
+  backgroundImage,
+  floatImage,
   buttonLabel,
   buttonLink
 }: HighLightProps) => (
-  <S.Wrapper>
-    <S.Title>{title}</S.Title>
-    <S.Subtitle>{subtitle}</S.Subtitle>
-    <Button as="a" href={buttonLink}>
-      {buttonLabel}
-    </Button>
+  <S.Wrapper backgroundImage={backgroundImage} alignment={alignment}>
+    {!!floatImage && <S.FloatImage src={floatImage} alt={title} />}
+    <S.Container>
+      <S.Title>{title}</S.Title>
+      <S.Subtitle>{subtitle}</S.Subtitle>
+      <Button as="a" href={buttonLink}>
+        {buttonLabel}
+      </Button>
+    </S.Container>
   </S.Wrapper>
 )
 
