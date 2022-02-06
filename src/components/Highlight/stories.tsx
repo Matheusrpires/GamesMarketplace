05e4fdb9d258df8995f49133705833c4676ca1c7
Matheus Rodrigues <matheusrp97@hotmail.com>
2022-02-06 16:36:25 -0300

    Highlight Adding unit tests and storybook story

--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -7,9 +7,23 @@ export default {
   args: {
     title: 'Read Dead it’s back',
     subtitle: 'Come see John’s new adventures',
+    backgroundImage: '/img/red-dead-img.jpg',
     buttonLabel: 'Buy now',
     buttonLink: '/rdr2'
   }
 } as Meta
 
-export const Default: Story<HighLightProps> = (args) => <Highlight {...args} />
+export const Default: Story<HighLightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+export const WithFloatImage: Story<HighLightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+WithFloatImage.args = {
+  floatImage: '/img/red-dead-float.png'
+}
--- src/components/Highlight/test.tsx
+++ src/components/Highlight/test.tsx
@@ -2,17 +2,19 @@ import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Highlight from '.'
+// import * as S from './styles'
 
 const props = {
   title: 'Heading 1',
   subtitle: 'Heading 2',
+  backgroundImage: '/img/red-dead-img.jpg',
   buttonLabel: 'Buy now',
   buttonLink: '/rdr2'
 }
 
 describe('<Highlight />', () => {
   it('should render the heading', () => {
-    renderWithTheme(<Highlight {...props} />)
+    const { container } = renderWithTheme(<Highlight {...props} />)
 
     expect(
       screen.getByRole('heading', { name: /heading 1/i })
@@ -21,5 +23,52 @@ describe('<Highlight />', () => {
       screen.getByRole('heading', { name: /heading 2/i })
     ).toBeInTheDocument()
     expect(screen.getByRole('link', { name: /buy now/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render background image', () => {
+    const { container } = renderWithTheme(<Highlight {...props} />)
+
+    expect(container.firstChild).toHaveStyle({
+      backgroundImage: `url(${props.backgroundImage})`
+    })
+  })
+
+  it('should render background image', () => {
+    renderWithTheme(<Highlight {...props} floatImage="/float-image.png" />)
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      '/float-image.png'
+    )
+  })
+
+  // it('should render in the right by default', () => {
+  //   const { container } = renderWithTheme(<Highlight {...props} />)
+
+  //   expect(container.firstChild).toHaveStyleRule(
+  //     'grid-template-area',
+  //     "'floatImage content'"
+  //   )
+
+  //   expect(container.firstChild).toHaveStyleRule('text-align', 'right', {
+  //     modifier: `${S.Container}`
+  //   })
+  // })
+
+  // it('should render in the left if prop is passed', () => {
+  //   const { container } = renderWithTheme(
+  //     <Highlight {...props} alignment="left" />
+  //   )
+
+  //   expect(container.firstChild).toHaveStyleRule(
+  //     'grid-template-areas',
+  //     "'content floatimage'"
+  //   )
+
+  //   expect(container.firstChild).toHaveStyleRule('text-align', 'left', {
+  //     modifier: `${S.Container}`
+  //   })
+  // })
 })
