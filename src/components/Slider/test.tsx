efdcadfa5d3974ea7d8c75da4911a7aca4fd51a6
Matheusrpires <matheusrp97@hotmail.com>
2022-03-01 19:08:28 -0300

    feat(slider): implement basic react slick slider

---
+++ src/components/Slider/test.tsx
@@ -0,0 +1,39 @@
+import { render, screen } from '@testing-library/react'
+import { Settings } from 'react-slick'
+import styled from 'styled-components'
+
+import Slider from '.'
+
+const settings: Settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1
+}
+
+const Slide = styled.div`
+  background: gray;
+  width: 30rem;
+  padding: 10rem 0;
+  border: 0.1rem solid red;
+  color: white;
+  text-align: center;
+`
+describe('<Slider />', () => {
+  it('should render the heading', () => {
+    const { container } = render(
+      <Slider settings={settings}>
+        <Slide>1</Slide>
+        <Slide>2</Slide>
+        <Slide>3</Slide>
+        <Slide>4</Slide>
+        <Slide>5</Slide>
+        <Slide>6</Slide>
+      </Slider>)
+
+    expect(screen.getByRole('heading', { name: /Slider/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
