7e59c98f54dbc96cad12120eee5615766b1133d4
Matheusrpires <matheusrp97@hotmail.com>
2022-03-01 19:58:02 -0300

    test(slider): Fixig matchmedia in js dom Due to jsdom not supporting matchmedia, it needs to create a mock

--- src/components/Slider/test.tsx
+++ src/components/Slider/test.tsx
@@ -1,39 +1,24 @@
+import 'match-media-mock'
 import { render, screen } from '@testing-library/react'
-import { Settings } from 'react-slick'
-import styled from 'styled-components'
 
 import Slider from '.'
 
-const settings: Settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 3,
-  slidesToScroll: 1
-}
-
-const Slide = styled.div`
-  background: gray;
-  width: 30rem;
-  padding: 10rem 0;
-  border: 0.1rem solid red;
-  color: white;
-  text-align: center;
-`
 describe('<Slider />', () => {
-  it('should render the heading', () => {
+  it('should render children as slider item', () => {
     const { container } = render(
-      <Slider settings={settings}>
-        <Slide>1</Slide>
-        <Slide>2</Slide>
-        <Slide>3</Slide>
-        <Slide>4</Slide>
-        <Slide>5</Slide>
-        <Slide>6</Slide>
+      <Slider settings={{ slidesToShow: 1, infinite: false }}>
+        <p>Item 1</p>
+        <p>Item 2</p>
       </Slider>
     )
 
-    expect(screen.getByRole('heading', { name: /Slider/i })).toBeInTheDocument()
+    expect(
+      screen.getByText(/item 1/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide')
+
+    expect(
+      screen.getByText(/item 2/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide')
 
     expect(container.firstChild).toMatchSnapshot()
   })
