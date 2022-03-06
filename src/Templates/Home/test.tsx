91efad062cadc8c990e0ea8e5fa288b42da11945
Matheusrpires <matheusrp97@hotmail.com>
2022-03-06 18:00:48 -0300

    Feat(HomePage)- Creat template for HomePage

---
+++ src/Templates/Home/test.tsx
@@ -0,0 +1,30 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Home from '.'
+
+describe('<Home />', () => {
+  it('should render the heading', () => {
+    renderWithTheme(<Home />)
+
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /contact us/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render all the headings', () => {
+    renderWithTheme(<Home />)
+
+    expect(screen.getByRole('heading', { name: /News/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Most Popular/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Upcomming/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Free Games/i })
+    ).toBeInTheDocument()
+  })
+})
