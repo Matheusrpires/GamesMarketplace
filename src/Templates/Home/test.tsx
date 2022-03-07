4ff1dd490824da6e5e3bd8aef5d8b11879d59532
Matheusrpires <matheusrp97@hotmail.com>
2022-03-07 20:37:08 -0300

    Feat(HomePage) - Adding hometamplete styles

--- src/Templates/Home/test.tsx
+++ src/Templates/Home/test.tsx
@@ -1,20 +1,33 @@
+import 'match-media-mock'
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Home from '.'
 
+import bannersMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
+const props = {
+  banners: bannersMock,
+  newReleases: [gamesMock[0]],
+  mostPopularHighLight: highlightMock,
+  mostPopularGames: [gamesMock[0]],
+  comingSoonGames: [gamesMock[0]],
+  comingSoonHighLight: highlightMock,
+  comingSoonMoreGames: [gamesMock[0]],
+  freeGamesHighLight: highlightMock,
+  freeGames: [gamesMock[0]]
+}
+
 describe('<Home />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<Home />)
+  it('should render the Home', () => {
+    renderWithTheme(<Home {...props} />)
 
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
     expect(
       screen.getByRole('heading', { name: /contact us/i })
     ).toBeInTheDocument()
-  })
-
-  it('should render all the headings', () => {
-    renderWithTheme(<Home />)
 
     expect(screen.getByRole('heading', { name: /News/i })).toBeInTheDocument()
     expect(
@@ -26,5 +39,12 @@ describe('<Home />', () => {
     expect(
       screen.getByRole('heading', { name: /Free Games/i })
     ).toBeInTheDocument()
+
+    // banner
+    expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
+    // card game ( 5 sections com 1 card cada = 5x1 = 5)
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(5)
+    // highlight
+    expect(screen.getAllByText(/Read Dead it’s back/i)).toHaveLength(3)
   })
 })
