23a0c87ff22fd8bd6f80aae6bd87c759f3c097e9
Matheusrpires <matheusrp97@hotmail.com>
2022-03-07 20:36:50 -0300

    Feat(HomePage) - Adding ServerSideProps for the home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,10 +13,7 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="Games Store"
-        />
+        <meta name="description" content="Games Store" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,24 @@
-import Home from '../Templates/Home'
+import Home, { HomeTemplateProps } from '../Templates/Home'
+import bannersMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
 
-export default function Index() {
-  return <Home />
+export default function Index(props: HomeTemplateProps) {
+  return <Home {...props} />
+}
+
+export function getServerSideProps() {
+  return {
+    props: {
+      banners: bannersMock,
+      newReleases: gamesMock,
+      mostPopularHighLight: highlightMock,
+      mostPopularGames: gamesMock,
+      comingSoonGames: gamesMock,
+      comingSoonHighLight: highlightMock,
+      comingSoonMoreGames: gamesMock,
+      freeGamesHighLight: highlightMock,
+      freeGames: gamesMock
+    }
+  }
 }
