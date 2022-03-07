561b1c4f87cdf8491fc83b5825d7b095552b5e30
Matheusrpires <matheusrp97@hotmail.com>
2022-03-07 20:35:25 -0300

    Feat(HomePage) - Adding components to home

--- src/Templates/Home/index.tsx
+++ src/Templates/Home/index.tsx
@@ -1,43 +1,91 @@
+import { BannerProps } from 'components/Banner'
+import BannerSlider from 'components/BannerSlider'
 import { Container } from 'components/Container'
 import Footer from 'components/Footer'
+import { GameCardProps } from 'components/GameCard'
+import GameCardSlider from 'components/GameCardSlider'
 import Heading from 'components/Heading'
+import Highlight, { HighLightProps } from 'components/Highlight'
 import Menu from 'components/Menu'
 
 import * as S from './styles'
 
-const Home = () => (
+export type HomeTemplateProps = {
+  banners: BannerProps[]
+  newReleases: GameCardProps[]
+  mostPopularHighLight: HighLightProps
+  mostPopularGames: GameCardProps[]
+  comingSoonGames: GameCardProps[]
+  comingSoonHighLight: HighLightProps
+  comingSoonMoreGames: GameCardProps[]
+  freeGamesHighLight: HighLightProps
+  freeGames: GameCardProps[]
+}
+
+const Home = ({
+  banners,
+  newReleases,
+  mostPopularHighLight,
+  mostPopularGames,
+  comingSoonGames,
+  comingSoonHighLight,
+  comingSoonMoreGames,
+  freeGames,
+  freeGamesHighLight
+}: HomeTemplateProps) => (
   <section>
     <Container>
       <Menu />
+      <S.SectionBanner>
+        <BannerSlider items={banners} />
+      </S.SectionBanner>
+    </Container>
+
+    <S.SectionNews>
+      <Container>
+        <Heading lineLeft lineColor="secondary" color="black">
+          News
+        </Heading>
+        <GameCardSlider items={newReleases} color="black" />
+      </Container>
+    </S.SectionNews>
+
+    <Container>
+      <S.SectionMostPopular>
+        <Heading lineLeft lineColor="secondary">
+          Most Popular
+        </Heading>
+        <Highlight {...mostPopularHighLight} />
+        <GameCardSlider items={mostPopularGames} />
+      </S.SectionMostPopular>
     </Container>
 
     <Container>
-      <Heading lineLeft lineColor="secondary" color="black">
-        News
-      </Heading>
+      <S.SectionUpcoming>
+        <Heading lineLeft lineColor="secondary">
+          Upcomming
+        </Heading>
+        <GameCardSlider items={comingSoonGames} />
+        <Highlight {...comingSoonHighLight} />
+        <GameCardSlider items={comingSoonMoreGames} />
+      </S.SectionUpcoming>
     </Container>
 
     <Container>
-      <Heading lineLeft lineColor="secondary">
-        Most Popular
-      </Heading>
+      <S.SectionFreeGames>
+        <Heading lineLeft lineColor="secondary">
+          Free Games
+        </Heading>
+        <Highlight {...freeGamesHighLight} />
+        <GameCardSlider items={freeGames} />
+      </S.SectionFreeGames>
     </Container>
 
-    <Container>
-      <Heading lineLeft lineColor="secondary">
-        Upcomming
-      </Heading>
-    </Container>
-
-    <Container>
-      <Heading lineLeft lineColor="secondary">
-        Free Games
-      </Heading>
-    </Container>
-
-    <Container>
-      <Footer />
-    </Container>
+    <S.SectionFooter>
+      <Container>
+        <Footer />
+      </Container>
+    </S.SectionFooter>
   </section>
 )
 
