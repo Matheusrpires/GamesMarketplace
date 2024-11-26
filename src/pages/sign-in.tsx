88c85c8bda37667b1e7df37f0d54e2757ca1cd8a
Matheusrpires <matheusrp97@hotmail.com>
2024-11-25 22:07:45 -0300

    feat: create sign in and up pages

---
+++ src/pages/sign-in.tsx
@@ -0,0 +1,5 @@
+import Auth from '../Templates/Auth'
+
+export default function SignIn() {
+  return <Auth title="Sign in">a</Auth>
+}
---
+++ src/pages/sign-up.tsx
@@ -0,0 +1,5 @@
+import Auth from '../Templates/Auth'
+
+export default function SignUp() {
+  return <Auth title="Sign Up">Children</Auth>
+}
